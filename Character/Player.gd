ba395e31228ac1c910969e8708b0e3b9a655c700
snenenenenenene <sennebels@gmail.com>
2023-05-01 22:33:46 +0200

    feat: :sparkles: following camera

--- Character/Player.gd
+++ Character/Player.gd
@@ -10,7 +10,7 @@ extends CharacterBody2D
 func _ready():
 	update_animation_parameters(starting_direction)
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	var input_direction = Vector2(
 		Input.get_action_strength("right") - Input.get_action_strength("left"),
 		Input.get_action_strength("down") - Input.get_action_strength("up")
