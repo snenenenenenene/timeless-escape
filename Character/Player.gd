3936ad6e84850904e26e458a555da15c7867698d
Lenny <lennyb.0908@gmail.com>
2023-05-04 21:05:29 +0200

    fixed animations

--- Character/Player.gd
+++ Character/Player.gd
@@ -1,33 +1,24 @@
 extends CharacterBody2D
 
+const MAX_SPEED = 100
 
-@export var move_speed : float = 100
-@export var starting_direction : Vector2 = Vector2(0,1)
+@onready var animationTree = $AnimationTree
+@onready var animationState = animationTree.get("parameters/playback")
 
-@onready var animation_tree = $AnimationTree
-@onready var state_machine = animation_tree.get("parameters/playback")
-
-func _ready():
-	update_animation_parameters(starting_direction)
-
-func _physics_process(_delta):
-	var input_direction = Vector2(
-		Input.get_action_strength("right") - Input.get_action_strength("left"),
-		Input.get_action_strength("down") - Input.get_action_strength("up")
-	)
+func _physics_process(delta):	
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
 	
-	update_animation_parameters(input_direction)
-	
-	velocity = input_direction * move_speed
-	move_and_slide()
-
-func update_animation_parameters(move_input : Vector2):
-	if(move_input != Vector2.ZERO):
-		animation_tree.set("parameters/Walk/blend_position", move_input)
-		animation_tree.set("parameters/Idle/blend_position", move_input)
-		
-func pick_new_state():
-	if(velocity != Vector2.ZERO):
-		state_machine.travel("Walk")
+	if input_vector != Vector2.ZERO:
+		animationTree.set("parameters/idling/blend_position", input_vector)
+		animationTree.set("parameters/running/blend_position", input_vector)
+		animationState.travel("running")
+		velocity = input_vector * MAX_SPEED
 	else:
-		state_machine.travel("Idle")
+		animationState.travel("idling")
+		velocity = Vector2.ZERO
+	
+	print("FINISH: %s" % velocity)
+	move_and_slide()
