24f0e381a73342bb3a34580c1741617bf15114cc
Lenny <lennyb.0908@gmail.com>
2023-05-06 14:47:28 +0200

    attempting merge

--- Character/Player.gd
+++ Character/Player.gd
@@ -1,44 +1,33 @@
 extends CharacterBody2D
 
-@export var move_speed : float = 100
-@export var starting_direction : Vector2 = Vector2(0,-0.1)
-
 @onready var dialogue = $Dialogue
-@onready var animation_tree = $AnimationTree
-@onready var state_machine = animation_tree.get("parameters/playback")
+
+const MAX_SPEED = 100
+@onready var animationTree = $AnimationTree
+@onready var animationState = animationTree.get("parameters/playback")
 
 func _ready():
 	dialogue.set("visible", false)
-	update_animation_parameters(starting_direction)
 
-func _physics_process(_delta):
-	var input_direction = Vector2(
-		Input.get_action_strength("right") - Input.get_action_strength("left"),
-		Input.get_action_strength("down") - Input.get_action_strength("up")
-	)
+func _physics_process(delta):	
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
 	
-	update_animation_parameters(input_direction)
-	
-	velocity = input_direction * move_speed
-	
-	move_and_slide()
-	
-	pick_new_state()
-
-func update_animation_parameters(move_input : Vector2):
-	if(move_input != Vector2.ZERO):
-		animation_tree.set("parameters/Walk/blend_position", move_input)
-		animation_tree.set("parameters/Idle/blend_position", move_input)
-
-func pick_new_state():
-	if(velocity != Vector2.ZERO):
-		state_machine.travel("Walk")
+	if input_vector != Vector2.ZERO:
+		animationTree.set("parameters/Idle/blend_position", input_vector)
+		animationTree.set("parameters/Walk/blend_position", input_vector)
+		animationState.travel("Walk")
+		velocity = input_vector * MAX_SPEED
 	else:
-		state_machine.travel("Idle")
-
+		animationState.travel("Idle")
+		velocity = Vector2.ZERO
+	
+	print("FINISH: %s" % velocity)
+	move_and_slide()
 
 func _on_actionable_finder_area_entered(area):
-	
 	var text = area.get("metadata/lights_dialogue_value")
 	var textField = $Dialogue/Text
 	textField.set("text", text)
